5e8d60f238f926b8e67a24411837ff63faad3de4
Anshidtp <shaanshid07@gmail.com>
2024-09-29 03:24:04 +0530

    prompt updated

--- app/rag/generator.py
+++ app/rag/generator.py
@@ -19,13 +19,16 @@ def generate_response(query: str, relevant_chunks: list) -> str:
     Generate a response using the OpenAI API based on the query and relevant chunks.
     """
     context = "\n".join(relevant_chunks)
-    prompt = f"Based on the following resume information:\n\n{context}\n\nAnswer the following question: {query}"
+    prompt = f"Based on the following  information:\n\n{context}\n\nAnswer the following question: {query}"
 
     
     response = client.chat.completions.create(
         model="llama3-8b-8192",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant that answers questions based on resume information."},
+            {"role": "system", "content": """You are a helpful assistant that answers questions based on the information.
+                                            if you dont't know the answer, just say i don't have knowledge about it, don't try to make up an answer,
+                                            Remember to keep your responses concise and Only return the helpful answer below and nothing else.
+                                            Answer :"""},
             {"role": "user", "content": prompt}
         ]
     )
